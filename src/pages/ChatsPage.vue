3adda1022334d40b6c8fd4de085ecd10f06366ef
Rob Molloy <robmolloy@hotmail.co.uk>
2023-05-30 23:59:24 +0100

    start chat layout

--- src/pages/ChatsPage.vue
+++ src/pages/ChatsPage.vue
@@ -1,8 +1,31 @@
-<template>Chats {{ $route.params.username }}</template>
+<template>
+  <q-footer class="q-pa-sm">
+    <q-input
+      dense
+      standout
+      bg-color="white"
+      rounded
+      model-value="text"
+      input-class="text-black"
+      label="input"
+    >
+      <template v-slot:append>
+        <q-icon
+          name="send"
+          @click="$emit('submit')"
+          class="cursor-pointer"
+          color="secondary"
+        />
+      </template>
+    </q-input>
+  </q-footer>
+</template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
 
+defineEmits(['submit']);
+
 const route = useRoute();
-console.log(/*LL*/ 9, 'route.params.username', route.params.username);
+console.log(/*LL*/ 9, 'route.params.id', route.params.id);
 </script>
